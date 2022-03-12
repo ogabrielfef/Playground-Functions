0894dcb9f478cc6fcc1bab2ff6bfc2f192fd3601
GabrielF <ogabrielfef@gmail.com>
2022-03-12 16:50:22 -0300

    aa

--- src/challenges.js
+++ src/challenges.js
@@ -81,16 +81,16 @@ console.log(catAndMouse(5,1,1));
 // Desafio 8
 function fizzBuzz(array) {
   let result = [];
-  let strin = "bug!"
+  let strin = 'bug!'
   for (let index = 0; index < array.length; index ++){
   if (array[index]% 3 === 0){
-    strin = "fizz"
+    strin = 'fizz'
   }
   if (array[index]% 5 === 0){
-    strin += "buzz"
+    strin += 'buzz'
   }
   if (array[index] % 3 === 0 && fizzBuzz[index]% 5 === 0){
-    strin += "fizzBuzz"
+    strin += 'fizzBuzz'
   } result.push(strin);
 } return result;
 }
