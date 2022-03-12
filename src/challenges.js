1387e71f538ed00f7023c4a12f15893528679658
GabrielF <ogabrielfef@gmail.com>
2022-03-12 15:43:00 -0300

    quase lá!

--- src/challenges.js
+++ src/challenges.js
@@ -48,27 +48,73 @@ console.log(footballPoints(1, 2));
 console.log(footballPoints(0, 0));
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayN) {
+  let ocorrencia = arrayN[0]
+  let vezes = 0;
+  for (let index = 0; index < arrayN.length; index++){
+    if(ocorrencia === arrayN[index]) vezes++;
+  }
+  return vezes;
 }
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs((mouse - cat1)) < Math.abs((mouse - cat2))){
+  return 'cat1';
+} 
+  if (Math.abs((mouse - cat1)) > Math.abs((mouse - cat2))){
+  return 'cat2';
+} if (Math.abs((mouse - cat1)) === Math.abs((mouse - cat2))){
+  return "os catos se trombam e o rato foge"
 }
+}
+console.log(catAndMouse(5,2,3));
+console.log(catAndMouse(7,1,19));
+console.log(catAndMouse(5,1,1));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let result = [];
+  for (let index = 0; index < array.length; index++){
+    let strin = "bug!"
+  if (array[index]% 3 === 0){
+    strin = "fizz"
+  }
+  if (array[index]% 5 === 0){
+    strin = "buzz"
+  }
+  if (array[index] % 3 === 0 && fizzBuzz[index]% 5 === 0){
+    strin = "fizzBuzz"
+  } result.push(strin);
+} return result;
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
+console.log(fizzBuzz([7, 9]));
+console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let trocaA = string.replace(/["a"]/gi, "1") ;
+  let trocaE = trocaA.replace(/["e"]/gi, "2") ;
+  let trocaI = trocaE.replace(/["i"]/gi, "3") ;
+  let trocaO = trocaI.replace(/["o"]/gi, "4") ;
+  let trocaU = trocaO.replace(/["u"]/gi, "5") ;
+  return trocaU
 }
-function decode() {
-  // seu código aqui
+console.log(encode("hi there!"));
+
+function decode(encodeString) {
+  let troca1 = encodeString.replace(/["1"]/gi, "a") ;
+  let troca2 = troca1.replace(/["2"]/gi, "e") ;
+  let troca3 = troca2.replace(/["3"]/gi, "i") ;
+  let troca4 = troca3.replace(/["4"]/gi, "o") ;
+  let troca5 = troca4.replace(/["5"]/gi, "u") ;
+  return troca5
 }
+console.log(decode("h3 th2r2!"));
 
 // Desafio 10
 function techList() {
