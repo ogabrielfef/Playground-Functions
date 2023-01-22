4dd44cc48a6a2f0054822ca6fa780cab39ec46cb
GabrielF <ogabrielfef@gmail.com>
2023-01-22 00:50:24 -0300

    adiciona README

--- src/challenges.js
+++ src/challenges.js
@@ -1,166 +1,168 @@
 // Desafio 1
-function compareTrue(primeiroTermo, segundoTermo){
-  if (primeiroTermo == true && segundoTermo == true){
-    return true;
-  } else {
-    return false;
-  }
+// implemente na função `compareTrue`, um código que ao receber dois parâmetros booleanos deve:
+// - Retornar `true` se ambos os valores forem verdadeiros;
+// - Retornar `false` se um ou ambos os parâmetros forem falsos.
+// Faça a função somente utilizando o operador `&&`.
+
+function compareTrue(primeiroTermo, segundoTermo) {
+//   if (primeiroTermo == true && segundoTermo == true){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+  return primeiroTermo && segundoTermo === true;
 }
+
 console.log(compareTrue(false, true));
 console.log(compareTrue(false, false));
 console.log(compareTrue(true, true));
 
 // Desafio 2
+// Escreva uma função com o nome `calcArea` que receba um valor de base (chamado `base`) e
+// outro de altura (chamado `height`) de um triângulo e retorne o cálculo da sua área.
+
 function calcArea(base, height) {
-  return (base * height)/2;
+  return (base * height) / 2;
 }
-console.log(calcArea(10, 50));
-console.log(calcArea(5, 2));
-console.log(calcArea(51, 1));
 
 // Desafio 3
+// Escreva uma função com o nome `splitSentence`, a qual receberá uma string e retornará uma
+// array de strings separadas por cada espaço na string original.
+
 function splitSentence(string) {
-  let array = string.split(" ");
+  let array = string.split(' ');
   return array;
 }
-console.log(splitSentence("go trybe"));
-console.log(splitSentence("vamo que vamo"));
-console.log(splitSentence("foguete"));
 
 // Desafio 4
+// Escreva uma função com o nome `concatName` que, ao receber uma array de strings, retorne uma
+// string com o formato `'ÚLTIMO ITEM, PRIMEIRO ITEM'`, independente do tamanho da array.
+
 function concatName(string) {
   let ultimoTerm = string[string.length - 1];
   let primeiroTerm = string[0];
-  return ultimoTerm + ", " + primeiroTerm;
+  return `${ultimoTerm}, ${primeiroTerm}`;
 }
-console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
-console.log(concatName(['foguete', 'não', 'tem', 'ré']));
-console.log(concatName(['captain', 'my', 'captain']));
 
 // Desafio 5
+// Escreva uma função com o nome `footballPoints` que receba o número de vitórias (esse parâmetro deverá se chamar `wins`) e
+// o número de empates (esse parâmetro deverá se chamar `ties`) e retorne a quantidade de pontos que o time marcou em um campeonato.
+// Para tanto, considere que cada vitória vale 3 pontos e cada empate vale 1 ponto.
+
 function footballPoints(wins, ties) {
-  let a = wins;
-  let b = ties;
-  return ((a * 3)+(b * 1));
+  // let a = wins;
+  // let b = ties;
+  return ((wins * 3) + (ties * 1));
 }
-console.log(footballPoints(14, 8));
-console.log(footballPoints(1, 2));
-console.log(footballPoints(0, 0));
 
 // Desafio 6
+// Escreva uma função chamada `highestCount` que, ao receber uma array de números, retorne a quantidade de vezes que o maior deles se repete.
 function highestCount(arrayN) {
   let ocorrencia = Math.max(...arrayN);
   let vezes = 0;
-  for (let index = 0; index < arrayN.length; index+= 1){
-    if(ocorrencia === arrayN[index]){
-    vezes += 1;
+  for (let index = 0; index < arrayN.length; index += 1) {
+    if (ocorrencia === arrayN[index]) {
+      vezes += 1;
     }
   }
   return vezes;
 }
-console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
-console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
-console.log(highestCount([0, 0, 0]));
 
 // Desafio 7
+// Imagine que existem dois gatos, os quais chamaremos de `cat1` e `cat2`, e que ambos estão caçando um mesmo rato chamado `mouse`. Imagine que
+// os animais estão em uma reta, cada um em uma posição representada por um número.
+
+// Sabendo disso, crie uma função chamada `catAndMouse` que, ao receber a posição de `mouse`, `cat1` e `cat2`, **nessa ordem**, calcule as
+// distâncias entre o rato e cada um dos gatos, em seguida, retorne qual dos felinos irá alcançar o rato primeiro (aquele que estiver mais perto do rato).
+
 function catAndMouse(mouse, cat1, cat2) {
-  if (Math.abs((mouse - cat1)) < Math.abs((mouse - cat2))){
-  return 'cat1';
-} 
-  if (Math.abs((mouse - cat1)) > Math.abs((mouse - cat2))){
-  return 'cat2';
-} 
-  if (Math.abs((mouse - cat1)) === Math.abs((mouse - cat2))){
-  return "os gatos trombam e o rato foge"
+  if (Math.abs((mouse - cat1)) < Math.abs((mouse - cat2))) {
+    return 'cat1';
+  }
+  if (Math.abs((mouse - cat1)) > Math.abs((mouse - cat2))) {
+    return 'cat2';
+  }
+  if (Math.abs((mouse - cat1)) === Math.abs((mouse - cat2))) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
-}
-console.log(catAndMouse(5,2,3));
-console.log(catAndMouse(7,1,19));
-console.log(catAndMouse(5,1,1));
 
 // Desafio 8
+// Crie uma função chamada `fizzBuzz` que receba uma array de números e retorne uma array da seguinte forma:
+// - Para cada número do Array que seja divisível apenas por 3, apresente uma string `"fizz"`;
+// - Para cada número do Array que seja divisível apenas por 5, apresente uma string `"buzz"`;
+// - Caso o número seja divisível por 3 e 5, retorne a string `"fizzBuzz"`;
+// - Caso o número não possa ser dividido por 3 nem por 5, retorne a string `"bug!"`;
+
 function fizzBuzz(array) {
   let result = [];
-  for (let index = 0; index < array.length; index ++){
-    if(array[index] % 3 === 0){
-      if(array[index] % 5 === 0){
-        result[index] = 'fizzBuzz';
-      }
-      else{
-        result[index] = 'fizz';
-      }
-    }
-    else if(array[index] % 5 === 0){
-      if(array[index] % 3 === 0){
-        result[index] = 'fizzBuzz';
-      }
-      else{
-        result[index] = 'buzz';
-      }
-    }
-    else{
-      result[index] = 'bug!';
-    }
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 15 === 0) result[i] = 'fizzBuzz';
+    else if (array[i] % 3 === 0) result[i] = 'fizz';
+    else if (array[i] % 5 === 0) result[i] = 'buzz';
+    else result[i] = 'bug!';
   }
-
   return result;
-
 }
-console.log(fizzBuzz([2, 15, 7, 9, 45]));
-console.log(fizzBuzz([7, 9]));
-console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
 function encode(string) {
-  let trocaA = string.replace(/["a"]/gi, "1") ;
-  let trocaE = trocaA.replace(/["e"]/gi, "2") ;
-  let trocaI = trocaE.replace(/["i"]/gi, "3") ;
-  let trocaO = trocaI.replace(/["o"]/gi, "4") ;
-  let trocaU = trocaO.replace(/["u"]/gi, "5") ;
-  return trocaU
+  let trocaA = string.replace(/["a"]/gi, '1');
+  let trocaE = trocaA.replace(/["e"]/gi, '2');
+  let trocaI = trocaE.replace(/["i"]/gi, '3');
+  let trocaO = trocaI.replace(/["o"]/gi, '4');
+  let trocaU = trocaO.replace(/["u"]/gi, '5');
+  return trocaU;
 }
-console.log(encode("hi there!"));
 
 function decode(encodeString) {
-  let troca1 = encodeString.replace(/["1"]/gi, "a") ;
-  let troca2 = troca1.replace(/["2"]/gi, "e") ;
-  let troca3 = troca2.replace(/["3"]/gi, "i") ;
-  let troca4 = troca3.replace(/["4"]/gi, "o") ;
-  let troca5 = troca4.replace(/["5"]/gi, "u") ;
-  return troca5
+  let troca1 = encodeString.replace(/["1"]/gi, 'a');
+  let troca2 = troca1.replace(/['2']/gi, 'e');
+  let troca3 = troca2.replace(/['3']/gi, 'i');
+  let troca4 = troca3.replace(/['4']/gi, 'o');
+  let troca5 = troca4.replace(/['5']/gi, 'u');
+  return troca5;
 }
-console.log(decode("h3 th2r2!"));
 
 // Desafio 10
+// Crie uma função que recebe um array de nomes de tecnologias que você quer aprender. Essa função deve receber também um segundo parâmetro
+// chamado `name` com um nome.
+// Para cada tecnologia no array, crie um objeto com a seguinte estrutura:
+
+// {
+//   tech: "NomeTech",
+//   name: name
+// }
+
+// Estes objetos devem ser inseridos em uma nova lista em ordem alfabética a partir do campo `tech` no objeto.
+// A saída da sua função deve ser uma lista de objetos ordenada pelo campo `tech` dos objetos com o formato acima.
 function techList(tech, nome) {
-  if (tech.length === 0) {
-    return 'Vazio!'
-  }
-  else {
-    let listArr = [];
+  let listArr = [];
+  if (tech.length !== 0) {
     tech.sort();
-    for (i in tech) {
+    for (let i = 0; i < tech.length; i += 1) {
       let results = {
         tech: tech[i],
-        name: nome
+        name: nome,
       };
       listArr.push(results);
     }
     return listArr;
   }
+  return 'Vazio!';
 }
-console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], "Lucas"));
 
 module.exports = {
-  calcArea,
-  catAndMouse,
   compareTrue,
+  calcArea,
+  splitSentence,
   concatName,
-  decode,
-  encode,
-  fizzBuzz,
   footballPoints,
   highestCount,
-  splitSentence,
+  catAndMouse,
+  fizzBuzz,
+  decode,
+  encode,
   techList,
 };
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,73 @@
-// Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+const MESAGE_1 = 'Array com tamanho incorreto.';
+const MESAGE_2 = 'não é possível gerar um número de telefone com esses valores';
+function lengthCheck(array) {
+  let result = '';
+  if (array.length !== 11) result = MESAGE_1;
+  return result;
+}
+function limitValueCheck(array) {
+  let result = '';
+  for (let num of array) {
+    if (num < 0 || num > 9) result = MESAGE_2;
+  }
+  return result;
 }
 
+function checkRepeat(array) {
+  let result = '';
+  // chek rule repeat 3 times or more
+  let numberArraySorted = array.slice(0);
+  numberArraySorted.sort(function (a, b) {
+    return a - b;
+  });
+  for (let i = 0; i <= 8; i += 1) {
+    if (numberArraySorted[i] === numberArraySorted[i + 2]) {
+      result = MESAGE_2;
+    }
+  }
+  return result;
+}
+
+// Desafio 11
+function generatePhoneNumber(array) {
+  let result = '';
+  result = lengthCheck(array);
+  if (result.length === 0) {
+    result = limitValueCheck(array);
+  }
+  if (result.length === 0) {
+    result = checkRepeat(array);
+  }
+  if (result.length === 0) {
+    // format number
+    let DDD = array.slice(0, 2).join('');
+    let firstPart = array.slice(2, 7).join('');
+    let secondPart = array.slice(7).join('');
+    result = `(${DDD}) ${firstPart}-${secondPart}`;
+  }
+  return result;
+}
+console.log(generatePhoneNumber([1, 1, 2, 4, 5, 6, 7, 8, 9, 0, 1]));
+
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let checkLineA = lineA < lineB + lineC && lineA > Math.abs(lineB - lineC);
+  let checkLineB = lineB < lineA + lineC && lineB > Math.abs(lineA - lineC);
+  let checkLineC = lineC < lineA + lineB && lineC > Math.abs(lineB - lineC);
+  return checkLineA && checkLineB && checkLineC;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(drink) {
+  let result = '';
+  let drinksTaken = drink.match(/\d+/g).map(Number);
+  let waterGlasses = drinksTaken.reduce((a, b) => a + b, 0);
+  if (waterGlasses === 1) {
+    result = `${waterGlasses} copo de água`;
+  } else {
+    result = `${waterGlasses} copos de água`;
+  }
+  return result;
 }
 
 module.exports = {
